cb684fc1b7ad69e724a9af0ef61138be492f2413
Lunara <the_wmw@protonmail.com>
2026-05-15 19:26:07 -0600

    maybe?

--- articles/1.js
+++
@@ -1,9 +0,0 @@
-fetch(`/articles/lunara/${id}.md`)
-  .then((res) => res.text())
-  .then((md) => {
-    const parts = md.split("---");
-
-    const content = parts[2] || md;
-
-    document.getElementById("article").innerHTML = marked.parse(content);
-  });
